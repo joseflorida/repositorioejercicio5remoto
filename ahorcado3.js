acfd75453933c300041a87a802ad4064844a68c1
ALUMNADO\joalga01 <josebeniparrell96@gmail.com>
2017-11-17 12:11:26 +0100

    subimos archivos

---
+++ ahorcado3.js
@@ -0,0 +1,17 @@
+var palabra0=[];
+
+var array1 = ["Coche","Moto","Portatil","Camara","Libro"];
+var aleatoria = Math.floor(Math.random()*(array1.length));
+var adivinar = array1[aleatoria]
+var condicion=false;
+
+for (var i = 0; i< adivinar.leght; i++) {
+  palabra0[i]="0";{
+    var letrauser=prompt("Dame una letra:");
+  }
+for (var i = 0; i < adivinar.leght; i++) {
+if (letrauser==adivinar.charAT(i)) {
+  palabra0[i]=letrauser;
+  condicion=true;
+
+}
